3f0e6ed9ec80791843c94c647186e13537c33c9c
hupeyy <huperw@ufl.edu>
2026-04-01 14:07:35 -0400

    fix: filled requirements.txt and validatation for env vars

--- config.py
+++ config.py
@@ -9,9 +9,11 @@ GCP_PROJECT_ID      = os.getenv('GCP_PROJECT_ID')
 DATASET_ID          = 'az_innovation_data'
 
 def validate_env():
-    missing = [
-        var for var in ['OPENWEATHER_API_KEY', 'GCP_PROJECT_ID']
-        if not os.getenv(var)
+    required = [
+        'OPENWEATHER_API_KEY',
+        'NEWS_API_KEY',
+        'GCP_PROJECT_ID',
     ]
+    missing = [var for var in required if not os.getenv(var)]
     if missing:
         raise EnvironmentError(f'Missing required environment variables: {missing}')
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from pipeline.runner import run_pipeline
+from config import validate_env
+
+from apis.weather.pipeline import get_api_meta as weather_meta
+from apis.news.pipeline    import get_api_meta as news_meta
 import apis.weather.pipeline as weather_api
 import apis.news.pipeline    as news_api
+from pipeline.runner import run_pipeline
 
-if __name__ == '__main__':
+def main():
+    validate_env()
     run_pipeline(weather_api)
     run_pipeline(news_api)
+
+
+if __name__ == '__main__':
+    main()
